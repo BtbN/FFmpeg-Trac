dccd73146da3b83169b8b5859b5e25a01cf5f5fb
BtbN <btbn@btbn.de>
2026-03-24 20:31:56 +0100

    Use proper selinux labels for volumes

--- init_home.sh
+++ init_home.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 ./build.sh
 
 mkdir -p home
-docker run --rm -v "$PWD/home:/home/trac" -u root ffmpeg_trac:latest sh -c "chown -R trac:trac /home/trac"
-docker run --rm -ti -v "$PWD/home:/home/trac" ffmpeg_trac:latest init
+docker run --rm -v "$PWD/home:/home/trac:z" -u root ffmpeg_trac:latest sh -c "chown -R trac:trac /home/trac"
+docker run --rm -ti -v "$PWD/home:/home/trac:z" ffmpeg_trac:latest init
